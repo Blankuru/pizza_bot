31308a01a431b3901f06e84029151f24b0899223
Blankuru <19227@my.sanctamaria.school.nz>
2022-02-15 10:28:18 +1300

    Update pizza_bot.py

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,1 +1,34 @@
 # Pizza Bot Program
+
+
+
+
+
+# Welcome message with random names from the list
+
+
+# Menu for pickup or delivery
+
+
+# Pick up information - Name and Phone number
+
+
+# Delivery Information - Name, Adress, Phone
+
+
+# Choose total number of pizzas - max 5
+
+
+# Pizza Menu
+
+
+# Pizza order - from menu - print each ordered with cost
+
+
+# Print order out - incl. of order is delivery or pickup and names and price of each pizza - total cost incl. any delivery charge 
+
+
+# Ability to cancel or proceed the order
+
+
+# Option for a new order or exit
