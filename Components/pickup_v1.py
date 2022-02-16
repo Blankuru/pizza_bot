2a203b75742d98601513a06b8f394ee3bfc9b258
Blankuru <19227@my.sanctamaria.school.nz>
2022-02-17 10:47:55 +1300

    added pickup_v1 and pickup_v2

---
+++ Components/pickup_v1.py
@@ -0,0 +1,11 @@
+
+print ("Please enter your pickup information")
+
+#Customer name
+name = input("Please enter your name: ")
+
+#Customer phone
+phone = input("Please enter your phone number: ")
+
+print (name)
+print (phone)
