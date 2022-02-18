07ba22060a833453a245554f78a2c6a9237ed106
Blankuru <19227@my.sanctamaria.school.nz>
2022-02-18 14:34:20 +1300

    added valid code

---
+++ Components/pickup_v2.py
@@ -0,0 +1,30 @@
+#Bug - accepts blank input
+
+print ("Please enter your pickup information")
+
+#Customer name
+valid = False
+while not valid:
+    name = input("Please enter your name: ")
+    print ("")
+    if name != "":
+        print (name)
+        break
+
+    else:
+        print ("Sorry this cannot be blank") 
+        
+#Customer phone
+valid = False
+while not valid:
+    phone = int(input("Please enter your phone number: "))
+    if phone != "":
+        print (phone)
+        break
+    
+    else:
+        print ("Sorry this cannot be blank")
+
+
+print (name)
+print (phone)
